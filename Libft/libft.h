0a860841eb8bd65f1af9489e62ac069826787f92
Heejune Cho <hecho@c1r7s1.42seoul.kr>
2020-10-05 16:27:33 +0900

    ft_strlcat

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hecho <hecho@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 15:10:10 by hecho             #+#    #+#             */
-/*   Updated: 2020/10/05 15:57:13 by hecho            ###   ########.fr       */
+/*   Updated: 2020/10/05 16:27:10 by hecho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
