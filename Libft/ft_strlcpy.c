c2c63e5dabd2eacaa9b24ad5188887689796e54d
Heejune Cho <hecho@c1r7s1.42seoul.kr>
2020-10-05 15:57:58 +0900

    ft_strlcpy

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -1,30 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hecho <hecho@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/05 15:10:10 by hecho             #+#    #+#             */
-/*   Updated: 2020/10/05 15:57:13 by hecho            ###   ########.fr       */
+/*   Created: 2020/10/05 15:51:08 by hecho             #+#    #+#             */
+/*   Updated: 2020/10/05 15:56:22 by hecho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t i;
+	size_t srcsize;
 
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char	*ft_strchr(const char *s, int c);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-
-#endif
+	i = 0;
+	while (src[i] && i + 1 < dstsize)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = 0;
+	srcsize = ft_strlen(src);
+	return (srcsize);
+}
